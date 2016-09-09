0c2f8da14d01ff528f1eb4cb14f39d076fd75579
Anders Geer Jakobsen <geer.anders@gmail.com>
2016-09-09 13:31:33 +0200

    Interfaces created

---
+++ src/CustomerInfoDTO.java
@@ -0,0 +1,7 @@
+/**
+ * Created by geera on 09-Sep-16.
+ */
+public interface CustomerInfoDTO
+{
+
+}
---
+++ src/IDateFerryDTO.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+/**
+ * Created by geera on 09-Sep-16.
+ */
+public interface IDateFerryDTO {
+
+}
